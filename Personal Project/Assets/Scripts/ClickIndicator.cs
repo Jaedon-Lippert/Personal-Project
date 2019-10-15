447d69fef39d93ade3ace690c7559fa84e553aa4
Jaedon <jaedonklb@gmail.com>
2019-10-15 05:05:54 -0600

    //Player Controller Complete, Color Indicator Created
    Created player physics
    Created a color indicator for when player is clicked

---
+++ Personal Project/Assets/Scripts/ClickIndicator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickIndicator : MonoBehaviour
+{
+    public Material myMat;
+    private Color defaultCol;
+    private bool touching;
+    public Color tColor;
+    // Start is called before the first frame update
+    void Start()
+    {
+        defaultCol = myMat.color;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseEnter()
+    {
+        touching = true;
+    }
+
+    private void OnMouseExit()
+    {
+        touching = false;
+    }
+
+    private void OnMouseDown()
+    {
+        if (touching) { myMat.color = tColor; }
+    }
+
+    private void OnMouseUp()
+    {
+        if(myMat.color == tColor) { myMat.color = defaultCol; }
+    }
+}
